468fc896a16f42e5f4ad7cb3cd952b44ad8c9280
jeff truong <jeffery.x.truong@gmail.com>
2023-10-16 16:44:43 -0700

    updating skeleton sample for abby

--- CafRatingApp/app.py
+++ CafRatingApp/app.py
@@ -5,13 +5,30 @@
 
 
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import time
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/s0', methods=['GET', 'POST'])
+def s0():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        print("inside s0 python function")
+        return redirect(url_for('home'))
+
+    # show the form, it wasn't submitted
+    return render_template('s0')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
