5f151654d832ec3eafa22394ea43a4ac45111064
Jeffery Truong <jeffery.truong@mediakind.com>
2023-10-16 02:12:14 -0700

    fixing issue, should work now with python3 QRCodeValidator.py

--- CafRatingApp/QRCodeValidator.py
+++ CafRatingApp/QRCodeValidator.py
@@ -3,24 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # Python program to Scan and Read a QR code
-from qrtools.qrtools import QR
-
-import QRCodeValidator
-
+# from qrtools import QR
+#import QRCodeValidator
+import cv2
+#from cv2 import cv2
+import numpy as np
 
 # IRCodeValidator.py
 
 # IRCodeValidator.read function controls the camera and read the qrcode. You should print out the QRCode.
-def read() -> bool:
+def read():
     # function body
-    my_QR = qrtools.QR()
-    my_QR.decode(filename="./static/Image/101520232252.png")
+    img = cv2.imread(r"./static/Image/101520232252.png")
+    detector = cv2.QRCodeDetector()
+    val, b, c = detector.detectAndDecode(img)
+    # my_QR = qrtools.QR()
+    # my_QR.decode(filename="./static/Image/101520232252.png")
 
     # decodes the QR code and returns True if successful
-    my_QR.decode()
+    # my_QR.decode()
 
     # prints the data
-    print(my_QR.data)
+    print(val)
     suc = False
     print("you are in IRCodeValidator.read")
 
@@ -31,6 +35,6 @@ def read() -> bool:
 
 
 if __name__ == '__main__':
-    QRCodeValidator.read()
+    read()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
