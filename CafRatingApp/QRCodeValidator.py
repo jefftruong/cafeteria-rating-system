b26a76f654e4375647d396c5009b48a072b00e91
Marcus Lam <marcuslam0710@gmail.com>
2023-10-16 00:04:11 -0700

    checking in first draft of qrcodevalidator

--- CafRatingApp/QRCodeValidator.py
+++ CafRatingApp/QRCodeValidator.py
@@ -2,6 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Python program to Scan and Read a QR code
+from qrtools.qrtools import QR
+
+import QRCodeValidator
 
 
 # IRCodeValidator.py
@@ -9,6 +13,14 @@
 # IRCodeValidator.read function controls the camera and read the qrcode. You should print out the QRCode.
 def read() -> bool:
     # function body
+    my_QR = qrtools.QR()
+    my_QR.decode(filename="./static/Image/101520232252.png")
+
+    # decodes the QR code and returns True if successful
+    my_QR.decode()
+
+    # prints the data
+    print(my_QR.data)
     suc = False
     print("you are in IRCodeValidator.read")
 
@@ -17,5 +29,8 @@ def read() -> bool:
 
     return suc
 
+
+if __name__ == '__main__':
+    QRCodeValidator.read()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
